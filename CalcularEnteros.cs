a5a313d4415abe10ba932d6ec4bc2b389eb7982a
Angel_Dario <114117203+Addev01@users.noreply.github.com>
2024-09-04 16:41:06 -0700

    Add files via upload

---
+++ CalcularEnteros.cs
@@ -0,0 +1,39 @@
+﻿
+namespace TareaWhiile.PpromedioAritmético
+{
+    public class CalcularEnteros
+    {
+        public void CalcularNumeros()
+        {
+            try
+            {
+                int suma = 0;
+                int contador = 0;
+                int numero = 0; ;
+                double promedio = 0;
+                while (contador < 10)
+                {
+                    Console.Write("Ingrese un numero entero: ");
+                    if (int.TryParse(Console.ReadLine(), out numero))
+                    {
+                        suma += numero;
+                        contador++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor, ingrese un número válido.");
+                    }
+                }
+
+                 promedio = (double)suma / 10;
+
+                Console.WriteLine($"La suma de los numeros es: {suma}");
+                Console.WriteLine($"El promedio aritmetico es: {promedio}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CalcularPiezas.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace TareaWhiile.TareasWhile
+{
+    public class CalcularPiezas
+    {
+        public void CalcularHierro()
+
+        {
+
+            try
+            {
+                int cantidadPiezas = 0;
+                int piezasAptas = 0;
+                double longitud = 0;
+                int contador = 0;
+
+                Console.WriteLine("Introduzca la cantidad de piezas de hierro :");
+                cantidadPiezas = int.Parse(Console.ReadLine());
+
+                
+                while (contador < cantidadPiezas)
+                {
+                    Console.WriteLine($"Introduzca la longitud de la pieza  de hierro{contador + 1}:");
+                    longitud = double.Parse(Console.ReadLine());
+
+                    // Verificar si la longitud esta en  rango entre 1.20 y 1.30
+                    if (longitud >= 1.20 && longitud <= 1.30)
+                    {
+                        piezasAptas++;
+                    }
+
+                    contador++;
+                }
+
+                Console.WriteLine($"Numero de piezas aptas : {piezasAptas}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Entrada invalida, favor de ingresar numeros correctamente");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hubo un error inesperado:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
