7a132476815c7f8a7bc86232c8df88298ead1f5c
Angel_Dario <114117203+Addev01@users.noreply.github.com>
2024-09-04 16:29:29 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+//using TareaWhiile.PpromedioAritmético;
+
+//CalcularEnteros CalcularEnteros = new CalcularEnteros();
+
+//CalcularEnteros.CalcularNumeros();
+
+using TareaWhiile.TareasWhile;
+
+CalcularPiezas CalcularPiezas = new CalcularPiezas();
+CalcularPiezas.CalcularHierro();
+
+
+
