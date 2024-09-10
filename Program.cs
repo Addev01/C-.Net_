999f5b33174769e96d5a0ad983fb64bbdffc0262
Angel_Dario <114117203+Addev01@users.noreply.github.com>
2024-09-10 16:03:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//using TareasBucleFor.DimensionesTriangulos;
+
+
+//CalcularDimensiones CalcularDimensiones = new CalcularDimensiones();
+//CalcularDimensiones.CalcularTriangulos();
+
+
+using TareasBucleFor.ultimos5;
+
+ContarDivisibles ContarDivisibles = new ContarDivisibles();
+ContarDivisibles.Contar();
+
+
+
