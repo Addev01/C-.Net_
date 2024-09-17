ab67eb23ddc97f4ff7eff007127726a3fddf9026
Angel_Dario <114117203+Addev01@users.noreply.github.com>
2024-09-17 16:28:37 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
-﻿using areaTrinagulo.Triangulos;
+﻿
+using SytemNomina.SueldoEmpleado;
 
-CalcularArea calcular = new CalcularArea();
+class ProgramaNomina
+{
+    static void Main(string[] args)
+    {
+        
+        List<Empleado> empleados = new List<Empleado>
+        {
+            new EmpleadoAsalariado("Angel", "Marte", "999-00-1221", 1000M),
+            new EmpleadoPorHoras("Ana", "Baez", "227-20-2242", 200M, 450M),
+            new EmpleadoPorComision("Ana", "Imenenez", "333-33-3333", 10000M, 0.06M),
+            new EmpleadoBaseMasComision("Juan", "Perez", "404-00-5964", 5000M, 0.04M, 300M)
+        };
 
-calcular.ProcesarTriangulos();
 
-
+        foreach (Empleado empleado in empleados)
+        {
+            Console.WriteLine(empleado);
+            Console.WriteLine($"Ingresos: {empleado.Ingresos():C}\n");
+        }
+    }
+}
 
